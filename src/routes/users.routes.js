16d529438cb61445e516380460d7709e5bd162b4
Anurag Pal <anuragpal7754@gmail.com>
2025-01-10 14:14:51 +0530

    added refreshtoken after it's expiry

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser } from "../controller/user.controller.js";
+import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controller/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,5 +29,8 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-router.route("/logout").post( verifyJWT,logOutUser)
+router.route("/logout").post( verifyJWT,logOutUser);
+
+router.route("/refresh-Token").post(refreshAccessToken);
+
 export default router;
